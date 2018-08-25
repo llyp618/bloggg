aa4f0d95051061533f473c245a9bda2360dfd445
llyp618 <llyp618@163.com>
2018-08-25 21:00:28 +0800

    config

---
+++ config/default.js
@@ -0,0 +1,12 @@
+module.exports = {
+	port:3000,
+	basePath:'/',
+	token:{
+		secret:'llyp618',
+		key:'lulublog',
+		expireTime:60 * 60 * 24 //一天
+	},
+	account:'601f1889667efaebb33b8c12572835da3f027f78',
+	password:'601f1889667efaebb33b8c12572835da3f027f78',
+	mongodb:'mongodb://localhost:27017/llll'
+}
